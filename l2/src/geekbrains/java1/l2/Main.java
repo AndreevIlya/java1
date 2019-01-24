872020f729dd36a0975d229220d2d7c2b1aea4f7
AndreevIlya <katauri@yandex.ru>
2019-01-24 23:47:08 +0300

    Task 1 done

--- l2/src/geekbrains/java1/l2/Main.java
+++ l2/src/geekbrains/java1/l2/Main.java
@@ -3,11 +3,35 @@ package geekbrains.java1.l2;
 public class Main {
 
     public static void main(String[] args) {
-        int[] c = arrayTest();
-        System.out.println(c[0]);
+        //Task 1
+        System.out.println("Task 1");
+        int[] array1 = {1,1,1,0,0,1,0,1,0,0,1,0};
+        arrayShow(array1);
+        arrayShow(zeroToOne(array1));
+        System.out.print("\n");
+
+        
     }
-    public static int[] arrayTest(){
-        int[][] a = {{1,2},{3,4}};
-        return a[1];
+    public static void arrayShow(int[] a){
+        for (int i=0; i < a.length; i++){
+            if (i == 0) System.out.print(a[i]);
+            else System.out.print(", " + a[i]);
+        }
+        System.out.print("\n");
     }
+    public static int[] zeroToOne(int[] a){
+        for (int i=0; i < a.length; i++){
+            switch (a[i]){
+                case 0:{
+                    a[i] = 1;
+                    break;
+                }case 1:{
+                    a[i] = 0;
+                    break;
+                }
+            }
+        }
+        return a;
+    }
+
 }
