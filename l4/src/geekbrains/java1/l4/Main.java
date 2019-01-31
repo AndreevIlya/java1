190c18c3569ddb7c64251509e548b15307eaeabe
AndreevIlya <katauri@yandex.ru>
2019-01-31 23:40:29 +0300

    Victory logic written to satisfy 1-3 Tasks

--- l4/src/geekbrains/java1/l4/Main.java
+++ l4/src/geekbrains/java1/l4/Main.java
@@ -4,8 +4,8 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
-    private static int SIZE = 3;
-    private static int DOTS_TO_WIN = 3;
+    private static int SIZE = 5;
+    private static int DOTS_TO_WIN = 5;
     private static char[][] map;
 
     private static final char DOT_EMPTY = '•';
@@ -18,6 +18,23 @@ public class Main {
     public static void main(String[] args) {
         initMap();
         printMap();
+        for(int i = 0; i < SIZE * SIZE; i++) {
+            humanTurn();
+            System.out.println("Ситуация после вашего хода:");
+            printMap();
+            if (checkWin(DOT_X)) {
+                System.out.println("Вы победили!");
+                break;
+            }
+            aiTurn();
+            System.out.println("Ситуация после хода компьютера:");
+            printMap();
+            if (checkWin(DOT_O)) {
+                System.out.println("Вы проиграли ;(");
+                break;
+            }
+            if (i == SIZE * SIZE - 1) System.out.println("Ничья ;(");
+        }
     }
 
     private static void initMap(){
@@ -41,5 +58,80 @@ public class Main {
             }
             System.out.println();
         }
+    }
+
+    private static void humanTurn() {
+        int x, y;
+        do {
+            System.out.println("Введите координаты в формате X (столбец) Y (строка)");
+            try {
+                x = sc.nextInt() - 1;
+                y = sc.nextInt() - 1;
+            }catch(Exception exc){
+                System.out.println("Только числа можно вводить!");
+                x = - 1;
+                y = - 1;
+            }
+        } while (!isCellValid(x, y));
+        map[y][x] = DOT_X;
+    }
+
+    private static boolean isCellValid(int x, int y) {
+        if (x < 0 || x >= SIZE || y < 0 || y >= SIZE) return false;
+        return (map[y][x] == DOT_EMPTY);
+    }
+
+    private static void aiTurn() {
+        int x, y;
+        do {
+            x = rand.nextInt(SIZE);
+            y = rand.nextInt(SIZE);
+        } while (!isCellValid(x, y));
+        System.out.println("Компьютер походил в точку " + (x + 1) + " " + (y + 1));
+        map[y][x] = DOT_O;
+    }
+    private static boolean checkWin(char letter) {
+        for(int i = 0; i < SIZE; i++) {// Check lines
+            int counter = 0;
+            for (int j = 0; j < SIZE; j++){
+                if (map[i][j] == letter) {
+                    counter++;
+                    //System.out.println(map[i][j] + " " + counter);
+                    if (counter == DOTS_TO_WIN) return true;
+                } else {
+                    counter = 0;
+                    //System.out.println(map[i][j] + " " + counter);
+                }
+            }
+        }
+        //System.out.println();
+        for(int i = 0; i < SIZE; i++){//Check columns
+            int counter = 0;
+            for(int j = 0; j < SIZE; j++) {
+                if (map[j][i] == letter) {
+                    counter++;
+                    //System.out.println(map[j][i] + " " + counter);
+                    if (counter == DOTS_TO_WIN) return true;
+                } else {
+                    counter = 0;
+                    //System.out.println(map[j][i] + " " + counter);
+                }
+            }
+        }
+        for(int i = 0; i <= SIZE - DOTS_TO_WIN; i++){
+            int counterL = 0;
+            int counterR = 0;
+            for(int j = i; j < SIZE; j++) {
+                if (map[j][j] == letter) {
+                    counterL++;
+                    if (counterL == DOTS_TO_WIN) return true;
+                } else counterL = 0;
+                if (map[SIZE - j - 1][j] == letter) {
+                    counterR++;
+                    if (counterR == DOTS_TO_WIN) return true;
+                } else counterR = 0;
+            }
+        }
+        return false;
     }
 }
